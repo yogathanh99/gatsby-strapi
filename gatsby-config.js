f0f7c125d456ccda9b05a33c56d6385b30cb15ce
yogathanh99 <vtthanh@apcs.vn>
2019-09-23 10:39:52 +0700

    Update config-gatsby.js and .gitignore file

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,9 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        apiURL: "http://localhost:1337",
+        apiURL: process.env.DEPLOY_URL
+          ? "https://cryptic-woodland-45585.herokuapp.com"
+          : "http://localhost:1337",
         contentTypes: [
           // List of the Content Types you want to be able to request from Gatsby.
           "article",
